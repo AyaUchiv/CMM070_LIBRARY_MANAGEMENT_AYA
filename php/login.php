76a5c6b6a42e9873ea0d2a6fd2f2140583579bfd
Ihotu00 <iifenne48@gmail.com>
2025-03-05 01:12:44 +0100

    fixed registration

--- php/login.php
+++ php/login.php
@@ -1,27 +1,37 @@
-<!DOCTYPE html>
-<html lang = "en">
+<?php
 
-<head>
-    <title>Login Page</title>
-</head>
+//when i submit my form
+if (isset($_POST["submit"])) {
+    $email = $_POST["email"];
+    $password = $_POST["password"];
+    $password_hidden = password_hash($password, PASSWORD_DEFAULT);
 
-<body>
-    <h3>Sign In</h3>
-        <form action = "connect.php" method = "post">
+    $error_message = array();
 
-            <label for = "email">Email:</label><br>
-            <input type = "email" name = "email" placeholder = "Email" required=""><br><br>
-
-            <label for = "password">Password:</label><br>
-            <input type = "password" name = "password" placeholder = "Password" required=""><br><br>
-
-            <input type = "submit" value = "Sign In">
-
-</form>
-</div>
-
-<p>Forgot Password? <a href = "#">Reset Here</a></p>
-<p>New User? <a href = "registration.php">&nbsp Register</a></p>
-</body>
-
-</html>
+    
+    if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+        array_push($error_message, "Email is not valid.");
+    }
+    if (strlen($password) < 8) {
+        array_push($error_message, "Password must not be less than 8 characters");
+    }
+    if (count($error_message) > 0) {
+        foreach ($error_message as $error) {
+            echo $error;
+        }
+    } else {
+        //user has successfully signed up
+        require_once "connect.php";
+        $sql = $conn->prepare("SELECT * FROM users WHERE Email = ? AND Password = ?");
+        $sql->bind_param("ss", $email, $password_hidden);
+        if ($sql->execute() === TRUE) {
+            echo "Logged in successfully";
+        } else {
+            echo "Error: " . $sql . "<br>" . $conn->error;
+        }
+        $sql->close();
+        header('Location: http://localhost:8080/html/index_page.html');
+        exit;
+    }
+}
+?>
--- php/registration.php
+++ php/registration.php
@@ -1,34 +1,15 @@
-<!DOCTYPE html>
-<html lang = "en">
+<?php
+require_once "connect.php";
+$sql = $conn->prepare("INSERT INTO users (Name, Email, Password)
+                VALUES (?, ?, ?)");
+$sql->bind_param("sss", $fullname, $email, $password_hidden);
+if ($sql->execute() === TRUE) {
+    echo "New record created successfully";
+} else {
+    echo "Error: " . $sql->error . "<br>" . $conn->error;
+}
+$sql->close();
+header('Location: http://localhost:8080/html/login.html');
+exit;
 
-<head>
-    <title>Register User</title>
-</head>
-
-<body>
-    <div class = "container">
-       
-        <h3>Register New User</h3>
-        <form action = "check_registration.php" method = "post">
-            <label for = "name">Full Name:</label><br>
-            <input type = "text" name = "full_name" placeholder = "Full Name" required = ""><br><br>
-
-            <label for = "email">Email:</label><br>
-            <input type = "email" name = "email" placeholder = "Email" required = ""><br><br>
-
-            <label for = "password">Password:</label><br>
-            <input type = "password" name = "password" placeholder = "Password" required = ""><br><br>
-
-            <label for = "repeat_password">Repeat Password:</label><br>
-            <input type = "password" name = "repeat_password" placeholder = "Password" required = ""><br>
-
-            <input type = "submit" value = "Sign Up">
-
-</form>
-</div>
-
-<p>Already a User? <a href = "login.php">&nbsp Login</a></p>
-
-</body>
-
-</html>
+?>
